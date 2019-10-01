8d5360f4727c850a91ed2cab055a2efdfee9c5fb
Jon Deer <40603806+JonDeerUa@users.noreply.github.com>
2019-10-01 13:54:45 +0300

    Update zoom.py

--- zoom.py
+++ zoom.py
@@ -362,7 +362,7 @@ def button_new(message):
         nw = 1
         h = open('n_status.txt', 'r')
         ds = h.read()
-        bot.send_message(message.chat.id, text.info_zak_no + ds)
+        bot.send_message(message.chat.id, text.info_zak_no + '/n' + ds)
         h.close()
     if b == '+':
         nw = 0
